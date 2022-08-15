0edba05ff65236ca5707fb7d5ed672b4e6b69087
Davi Banfi <davibanfi1579@outlook.com>
2022-08-15 12:15:56 -0300

    fix: add default values

--- src/presentation/serializers/post/FindAllPostsValidator.ts
+++ src/presentation/serializers/post/FindAllPostsValidator.ts
@@ -1,16 +1,16 @@
-import { IsNumber, IsPositive, Max } from "class-validator";
+import { IsNumber, IsOptional, IsPositive, Max } from "class-validator";
 import AbstractSerializer from "../AbstractSerializer";
 
 export type InputFindAllPostsController = {
-  page: string;
-  limit: string;
+  page?: string;
+  limit?: string;
 };
 
 export default class FindAllPostsValidator extends AbstractSerializer<InputFindAllPostsController> {
   constructor(data: InputFindAllPostsController) {
     super(data);
-    this.page = +data.page;
-    this.limit = +data.limit;
+    this.page = data.page ? +data.page : 0;
+    this.limit = data.limit ? +data.limit : 10;
   }
 
   @IsNumber()
